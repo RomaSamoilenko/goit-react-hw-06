e06a354062ce983f7f3adac646e6a3391c8a15d6
RomaSamoilenko <107999000+RomaSamoilenko@users.noreply.github.com>
2024-05-04 15:13:09 +0300

    3

---
+++ src/redux/contactsSlice.js
@@ -0,0 +1,47 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { nanoid } from "nanoid";
+
+const initialState = {
+  items: [
+    { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+    { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+    { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+    { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+  ],
+};
+
+const contactsSlice = createSlice({
+  name: "contacts",
+  initialState,
+  reducers: {
+    addContact: {
+      reducer: (state, action) => {
+        state.items.push({
+          name: action.payload.value.name,
+          number: action.payload.value.number,
+          id: action.payload.id,
+        });
+      },
+      prepare: (value) => {
+        return {
+          payload: {
+            value,
+            id: nanoid(),
+          },
+        };
+      },
+    },
+
+    deleteContact(state, action) {
+      state.items = state.items.filter(
+        (contact) => contact.id !== action.payload
+      );
+    },
+  },
+});
+
+export const { addContact, deleteContact } = contactsSlice.actions;
+
+export const selectContacts = (state) => state.contacts.items;
+
+export default contactsSlice.reducer;
